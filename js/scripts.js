c82dbea02adc134a3d731f8a5ad0dfaec7a48a27
Whitney Lugo <107445438+WhitneyL808@users.noreply.github.com>
2023-06-15 04:36:19 +0000

    new styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,32 @@
-// // My Scripts
-// Data source for roster of students
+// Data source
 const dataSource = "https://assets.codepen.io/16425/spring23web3.json";
 
-//get DOM elements
-const figcaption = document.querySelector('figcaption');
-const img = document.querySelector('figure img'); 
-const grid = document.querySelector(".grid");
-
-// fetch the data and set it 
+// fetch
 fetch( dataSource )
-  .then( response  => response.json())
-  .then( data  => {
-  
-  const roster = document.querySelector(".roster");
-  
-      
-    // console.log( data );
-//   figcaption.textContent = data[4].name
-//   img.src = data[4].Image;
-  
-   data.forEach((student) => {
-     
-       // template for js to create 
-    const template = `
-    <body>
-    <main class="roster">
-      <section>
-      <nav class="grid">
-      <ul>
-      
-      <li>
-      
-           <figcaption>${student.Name}</figcaption>
-           <img src="${student.Image}" alt="Student Name"></img>
-           <p>${student.Quote}</p>
-           
-      </li>
-        
-      
-      </ul>
-      </nav>
-      </section>
-      </main>
-      </body>
+.then( response => response.json())
+.then( data  => {
 
-    `; 
-  
-  // put template on page 
-  roster.insertAdjacentHTML('afterbegin' , template); 
-     
-   });
+    // get container for data
+    const roster = document.querySelector(".roster");
+ 
+    // for each RECORD, insert into parent
+    data.forEach( (student) => { 
+      
+      // template to reuse
+      const template = `
+        <figure style="background: ${student.Color}">
+         <figcaption> ${student.Name} </figcaption>
+            <img src="${student.Image}" alt="Student Name">
+         </figure>
+      `
     
-
+      roster.insertAdjacentHTML('afterbegin', template );
+      
+    });
+  
 });
 
-// console.log( data );
-    // console.log( data[4] );
-    // console.log( data[4].Name );
-    // console.log( data[4].Image );
-    // console.log( data[4].Quote );
+    // console.log(data);
+    // console.log(data[9].Superpower);
+    // console.log(data[2].Emoji);
+    // console.log(data[1].Quote);
